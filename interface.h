6fe67ae42175fe88ee59ec2ade3c45dd79f4e813
Julien ZHU <julien.zhu@pc131-07.pedago.ensiie.fr>
2022-03-04 17:35:13 +0100

    Tache A.4 : Création du fichier interface.h

---
+++ interface.h
@@ -0,0 +1,14 @@
+/*
+@requires : ???
+@assigns : nothing ?
+@ensures : affiche le plateau (wip)
+*/
+void showBoard(/**/);
+
+/*
+@requires : ???
+@assigns : nothing ?
+@ensures : affiche le plateau (wip)
+*/
+void showHand(/**/);
+
