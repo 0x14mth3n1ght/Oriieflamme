dafa21cb85595f6cc0ef8b7b3fd6120cdba81bb6
@alexandre.maroussiak <alexandre.maroussiak@ensiie.fr>
2022-03-17 17:09:30 +0100

    reduction des commentaires et ajout des entetes define, ifndef et endif

--- faction.h
+++ faction.h
@@ -1,14 +1,11 @@
-/* fichier faction.h */
+#ifndef _FACTION_H
+#define _FACTION_H
 
 /**
 * \struct ceci est une declaration d'un type abstrait
 * faction, lors de l'implementation, il faudra specifier
 * les differents champs du type faction 
 * (une faction sera donc caracterisee par un tuple)
-* \attention j'attire l'attention sur le fait qu'il ne faudra
-* pas oublier d'inclure certains champs lors de l'implementation afin 
-* que les fonctions/procedures suivantes fonctionnent correctement
-* en particulier des champs "main" et "pioche"
 */
 typedef struct faction_base *faction;
 
@@ -31,7 +28,7 @@ int a_remelanger_main(faction f);
 * remettre sa main dans la boite a idee, la melanger,
 * et repiocher 8 cartes
 * \param fp un pointeur vers une faction (ici il y aura
-* \a modification des champs "main" et "pioche" de la faction)
+* \a modification des champs de la faction)
 * \attention 1) le pointeur doit pointer vers une faction existante
 * 2) si la faction a deja utilise l'option de remelanger sa main, 
 * elle ne peut pas le refaire
@@ -44,7 +41,7 @@ void remelanger_main(faction *f);
 * main dans sa pioche. A la fin de la procedure
 * la main de la faction est donc vide et la pioche modifiee
 * \param fp un pointeur vers une faction (ici il y aura 
-* \a modification des champs "main" et "pioche" de la faction)
+* \a modification des champs de la faction)
 * \attention 1) le pointeur doit pointer vers une faction existante
 * 2) la main ne doit pas etre deja vide
 * \return la procedure ne retourne rien
@@ -54,7 +51,7 @@ void vider_main(faction *f);
 /**
 * \brief cette procedure permet a une faction de melanger sa pioche
 * \param fp un pointeur vers une faction (ici il y aura 
-* \a modification du champ "pioche" de la faction)
+* \a modification des champs de la faction)
 * \attention 1) le pointeur doit pointer vers une faction existante
 * \return la procedure ne retourne rien
 */
@@ -63,9 +60,11 @@ void melanger_pioche(faction *f);
 /**
 * \brief cette procedure permet a une faction de repiocher ses cartes
 * \param fp un pointeur vers une faction (ici il y aura
-* \a modification du champ "main" et "pioche" de la faction)
+* \a modification des champs de la faction)
 * \attention 1) le pointeur doit pointer vers une faction existante
 * 2) la main doit etre initialement vide
 * \return la procedure ne retourne rien
 */
 void repiocher(faction *f);
+
+#endif
