de3169b0ef23285a00437150fc29c2bce7840300
Julien ZHU <julien.zhu@pc131-18.pedago.ensiie.fr>
2022-04-01 17:50:41 +0200

    Tache B.3 : Modif debut_grille

--- structure.c
+++ structure.c
@@ -1,9 +1,10 @@
+#include <stdlib.h>
 #include "structure.h"
 
-/*Grille : liste quadruplement chaînée*/
+/*--------------------------------- Grille : liste quadruplement chaînée ---------------------------------*/
 
-struct cell {
-    int id_carte;
+struct cell_base {
+    carte card;
     int visible;    /* 0 : cachée, 1 : visible */
     int activated;  /* 0 : non-activée, 1 : activée */
     int i;          /* Cellule de l'abscisse de la grille (pourra etre négatif) */
@@ -14,7 +15,24 @@ struct cell {
     cell* south;    /* Pointeur vers la carte au sud    */
 };
 
- /*Pioche : liste chaînée*/
+grille debut_grille(carte card){
+    grille res = malloc(sizeof(grille));
+    res->card = card;
+    res->visible = 0;
+    res->activated = 0;
+    res->i = 0;
+    res->j = 0;
+    cell* west = NULL;
+    cell* north = NULL;
+    cell* east = NULL;
+    cell* south = NULL;
+}
+/*
+void pointer_to(cell* c, int direction, int i, int j){
+    c->
+}*/
+
+ /*--------------------------------- Pioche : liste chaînée ---------------------------------*/
 
 struct bucket { /*Maillon*/
     carte card;
--- structure.h
+++ structure.h
@@ -10,26 +10,26 @@
 typedef struct cell_base cell;
 typedef cell* grille;
 
-/*  @requires nothing
-    @assigns nothing
-    @ensures retourne une pioche vide */
-grille cree_grille_vide();
+/*  @requires : card est valide
+    @assigns  : nothing
+    @ensures  : retourne une grille vide avec la cellule de coordonnées (0,0) ayant la carte c dessus,*/
+grille debut_grille(carte card);
 
-/*  @requires g valide
-    @assigns g
-    @ensures retourne la cellule de coordonnées (0,0) ayant la carte c dessus, que la grille g pointera
-*/
-cell first_card(grille g, carte c);
-
-/*  @requires c cellule de la grille valide, direction entre 1 et 4
-    @assigns c
-    @ensures le pointeur c->direction devient la cellule ayant les coordonnées (i,j)
+/*  @requires : c cellule de la grille valide, direction entre 1 et 4
+    @assigns  : c
+    @ensures  : le pointeur c->direction devient la cellule ayant les coordonnées (i,j)
 */
 void pointer_to(cell* c, int direction, int i, int j);
 
-/*  @requires c est une cellule valide, c->direction est libre
-    @assigns c
-    @ensures retourne -1 si c->direction n'est pas libre ; 0 si la carte a été placée
+/*  @requires : nothing
+    @assigns  : nothing
+    @ensures  : retourne la cellule qui a pour coordonnées (i,j) ; error si aucune cellule n'a ces coordonnées
+*/
+cell find_cell(int i, int j);
+
+/*  @requires : c est une cellule valide, c->direction est libre
+    @assigns  : c
+    @ensures  : retourne -1 si c->direction n'est pas libre ; 0 si la carte a été placée
 */
 int place_card_next_to(cell* c, int direction, carte card);
 
@@ -39,30 +39,30 @@ typedef carte* carteEnMain; /*Tableau 1D de cartes*/
 /*---------------------------------Pioche---------------------------------*/
 typedef struct bucket* pioche;
 
-/*  @requires nothing
-    @assigns nothing
-    @ensures retourne une pioche vide */
+/*  @requires : nothing
+    @assigns  : nothing
+    @ensures  : retourne une pioche vide */
 pioche cree_pioche_vide();
 
-/*  @requires l est une pioche valide
-    @assigns nothing
-    @ensures retourne 1 si l est vide et 0 sinon */
+/*  @requires : l est une pioche valide
+    @assigns  : nothing
+    @ensures  : retourne 1 si l est vide et 0 sinon */
 int est_vide(pioche l);
 
-/*  @requires pl est un pointeur valide vers une pioche valide
-    @assigns *pl
-    @ensures ajoute c au dessus de la pioche pl */
+/*  @requires : pl est un pointeur valide vers une pioche valide
+    @assigns  : *pl
+    @ensures  : ajoute c au dessus de la pioche pl */
 void push(carte c, pioche *pl);
 
-/*  @requires pl est un pointeur valide vers une pioche valide non-vide
-    @assigns *pl
-    @ensures retourne la tête de la pioche et la retire de la pioche */
+/*  @requires : pl est un pointeur valide vers une pioche valide non-vide
+    @assigns  : *pl
+    @ensures  : retourne la tête de la pioche et la retire de la pioche */
 carte pop(pioche *pl);
 /*pop = tirer une carte*/
 
-/*  @requires l est une pioche valide
-    @assigns nothing
-    @ensures print la pioche */
+/*  @requires : l est une pioche valide
+    @assigns  : nothing
+    @ensures  : print la pioche */
 void print_pioche(pioche l);
 
 #endif
