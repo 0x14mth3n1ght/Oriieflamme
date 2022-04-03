cd7fb37e75a47e78aed81ef9623dc62d416a4818
Julien Zhu <>
2022-04-03 19:01:59 +0200

    Tache B3 : .h de la structure de plateau ; de la main ; de la pioche ;; .c : implementation des fonctions de la structure de la main et de la pioche (partielle)

--- structure.c
+++ structure.c
@@ -1,40 +1,174 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+
 #include "structure.h"
 
-/*--------------------------------- Grille : liste quadruplement chaînée ---------------------------------*/
+/*----------- Plateau = Grille : liste doublement chainee imbriquee dans liste doublement chainee -----------*/
 
-struct cell_base {
-    carte card;
-    int visible;    /* 0 : cachée, 1 : visible */
-    int activated;  /* 0 : non-activée, 1 : activée */
-    int i;          /* Cellule de l'abscisse de la grille (pourra etre négatif) */
-    int j;          /* Cellule de l'ordonnée de la grille (pourra etre négatif) */
-    cell* west;     /* Pointeur vers la carte à l'ouest */
-    cell* north;    /* Pointeur vers la carte au nord   */
-    cell* east;     /* Pointeur vers la carte à l'est   */
-    cell* south;    /* Pointeur vers la carte au sud    */
+/*----Liste doublement chainee mere----*/
+//Liste doublement chaine mere = Pointeur vers un maillon "colonne"
+typedef struct plateau_base colonne;
+typedef colonne* ldc_mere;
+
+struct plateau_base {
+    //Element de la liste doublement chainee
+    ldc_fille ligne;
+    int y; //ordonnée de la colonne (offset_from_center : 0 si centre, 1 si colonne d'en dessous, -1 si colonne d'au dessus etc)
+    //Pointeurs vers le "maillon" precedent ou suivant
+    ldc_mere north;
+    ldc_mere south;
 };
 
-grille debut_grille(carte card){
-    grille res = malloc(sizeof(grille));
-    res->card = card;
-    res->visible = 0;
-    res->activated = 0;
-    res->i = 0;
-    res->j = 0;
-    cell* west = NULL;
-    cell* north = NULL;
-    cell* east = NULL;
-    cell* south = NULL;
+/*----Liste doublement chainee fille----*/
+typedef struct maillon * ldc_fille;
+typedef struct maillon cell;
+
+struct maillon {
+    //Elements de la liste doublement chainee
+    carte card;             //carte posee sur ces coordonnees (x,y)
+    int x; //abscisse de la ligne (offset_from_center : 0 si centre, 1 si case a droite, -1 si case a gauche etc)
+    int is_visible;         //0 si la carte est face cachée ; 1 si la carte est retournée
+    int is_activated;       //1 si l'effet de la carte a été activée ; 0 sinon
+    //Pointeurs vers le "maillon" precedent ou suivant
+    ldc_fille west;
+    ldc_fille east;
+};
+
+/*----Fonctions----*/
+plateau init_grille(carte c){
+    plateau out;
+    ldc_fille ini;
+        ini->card = c;
+        ini->x = 0;
+        ini->is_visible = 0;
+        ini->is_activated = 0;
+        ini->west = NULL;
+        ini->east = NULL;
+    out->ligne = ini;
+    out->y = 0;
+    out->north = NULL;
+    out->south = NULL;
+    return out;    
 }
-/*
-void pointer_to(cell* c, int direction, int i, int j){
-    c->
-}*/
 
- /*--------------------------------- Pioche : liste chaînée ---------------------------------*/
+/*wip*/
 
-struct bucket { /*Maillon*/
+/*--------------------------------- Cartes en main : Fonctions ---------------------------------*/
+void main_vide(carteEnMain cmain, int size){
+    for (int i=0; i<size; i++){
+        cmain[i] = /*wip : carte_nulle*/NULL ;
+    }
+}
+
+int ajout_carte(carte c, carteEnMain cmain, int size){
+    int i = 0;
+    while (i<size){
+        i++;
+        if (cmain[i] == /*wip : carte_nulle*/NULL){
+            cmain[i] = c;
+            return i;
+        }
+    }
+    //main pleine
+    //assert(i == size-1);
+    return 0;
+}
+
+int indice_carte_main(carte c, carteEnMain cmain, int size){
+    int i=0;
+    while (i<size){
+        if (cmain[i] == c)
+            return i;
+        i++;
+    }
+    //non trouvé
+    return -1;
+}
+
+int supprime_carte_main(carte c, carteEnMain cmain, int size){
+    if (!(mem_main(c, cmain, size)))
+        return 0;
+    int i = 0;
+    int deleted = 0;
+    while (i<size - 1){
+        if (cmain[i] == c)
+            deleted = 1;
+        if (deleted)
+            cmain[i] == cmain[i+1]; //carte nulle si le prochain est carte_nulle.
+        i++;
+    }
+    //Cas la carte etait en derniere position ou si elle a déjà été supprimée
+    if (cmain[size-1] == c || deleted)
+        cmain[size-1] = /*wip : carte_nulle*/NULL;
+
+    return 1;
+}
+
+carte get_carte_at(int pos, carteEnMain cmain, int size){
+    return cmain[pos];
+}
+
+/*--------------------------------- Pioche : liste chaînée ---------------------------------*/
+struct bucket {
     carte card;
     pioche next;
 };
+
+/*----Fonctions----*/
+pioche creer_pioche_vide(){
+    return NULL;
+}
+
+int est_vide(pioche p){
+    return (p==NULL);
+}
+
+/*  @requires : p est une pioche valide, c une carte valide
+    @assigns  : nothing
+    @ensures  : ajoute c au dessus de la liste p */
+pioche cons(carte c, pioche p){
+    pioche res = malloc(sizeof(pioche));
+    res->card = c;
+    res->next = p;
+    return res;
+}
+
+void push(carte c, pioche *pp){
+    *pp = cons(c, *pp);
+}
+
+carte pop(pioche* pp){
+    if (*pp == NULL){
+        printf("Error pop : la pioche est vide.\n");
+        exit(EXIT_FAILURE);
+    }
+    carte out = (*pp)->card;
+    *pp = (*pp) -> next;
+    return out;
+}
+
+carte peek(pioche p){
+    if (p == NULL){
+        printf("Error peek : la pioche est vide.\n");
+        exit(EXIT_FAILURE);
+    }
+    return p->card;
+}
+
+int mem_pioche(pioche p, carte c){
+    while (p!=NULL){
+        if (p->card == c) return 1;
+        p = p->next;
+    }
+    return 0;
+}
+
+int pioche_len(pioche p){
+    int len = 0;
+    while (p!=NULL){
+        len++;
+        p = p->next;
+    }
+    return len;
+}
--- structure.h
+++ structure.h
@@ -6,63 +6,97 @@
 #include "faction.h"
 #include "plateau.h"
 
-/*---------------------------------Grille---------------------------------*/
-typedef struct cell_base cell;
-typedef cell* grille;
+/*----------- Plateau = Grille : liste doublement chainee imbriquee dans liste doublement chainee -----------*/
 
-/*  @requires : card est valide
+/*  @requires : c est une carte valide
     @assigns  : nothing
-    @ensures  : retourne une grille vide avec la cellule de coordonnées (0,0) ayant la carte c dessus,*/
-grille debut_grille(carte card);
+    @ensures  : retourne le plateau contenant la carte c au centre */
+plateau init_grille(carte c);
 
-/*  @requires : c cellule de la grille valide, direction entre 1 et 4
-    @assigns  : c
-    @ensures  : le pointeur c->direction devient la cellule ayant les coordonnées (i,j)
-*/
-void pointer_to(cell* c, int direction, int i, int j);
-
-/*  @requires : nothing
+/*  @requires : g est un plateau valide
     @assigns  : nothing
-    @ensures  : retourne la cellule qui a pour coordonnées (i,j) ; error si aucune cellule n'a ces coordonnées
-*/
-cell find_cell(int i, int j);
+    @ensures  : retourne 1 si la case (x,y) n'est pas occupée, 0 sinon */
+int est_libre(plateau g, int x, int y);
 
-/*  @requires : c est une cellule valide, c->direction est libre
-    @assigns  : c
-    @ensures  : retourne -1 si c->direction n'est pas libre ; 0 si la carte a été placée
-*/
-int place_card_next_to(cell* c, int direction, carte card);
+/*  @requires : pg est un pointeur valide vers un plateau valide, c est une carte valide
+    @assigns  : *pg
+    @ensures  : place la carte c sur la case (x,y) de la plateau si la case n'est pas occupée, retourne 1 le cas échéant. retourne 0 sinon */
+int placer_carte(carte c, int x, int y, plateau* pg);
+
+/*  @requires : g est un plateau valide
+    @assigns  : nothing
+    @ensures  : retourne la carte située aux coordonnées (x,y) du plateau g ; s'il n'y a pas de carte, renvoie la carte nulle*/
+carte get_carte(plateau g, int x, int y);
+
+/*  @requires : pg est un pointeur valide vers un plateau valide
+    @assigns  : *pg
+    @ensures  : supprime la carte du plateau située aux coordonnées (x,y) du plateau *pg (remplacée par la carte nulle), retourne 1 le cas échéant. retourne 0 sinon (pas de carte ou carte déjà supprimée) */
+int supprime_carte_plateau(plateau *pg, int x, int y);
 
 /*---------------------------------Cartes en main---------------------------------*/
-typedef carte* carteEnMain; /*Tableau 1D de cartes*/
+typedef carte* carteEnMain; /*Tableau 1D de cartes ; un emplacement vide sera caractérisé par la carte nulle*/
+
+/*  @requires : cmain tableau de taille size
+    @assigns  : cmain
+    @ensures  : initialise une main vide composée de size cartes */
+void main_vide(carteEnMain cmain, int size);
+
+/*  @requires : cmain tableau de taille size
+    @assigns  : cmain
+    @ensures  : rajoute la carte c à la main cmain si elle n'est pas pleine, renvoie sa position dans cmain le cas échéant. renvoie 0 si la main était pleine */
+int ajout_carte(carte c, carteEnMain cmain, int size);
+
+/*  @requires : cmain tableau de taille size
+    @assigns  : nothing
+    @ensures  : teste si la carte c est dans la main cmain, renvoie la position de la premiere occurence de la carte le cas échéant. renvoie -1 sinon */
+int indice_carte_main(carte c, carteEnMain cmain, int size); //sert de mem
+
+/*  @requires : cmain tableau de taille size
+    @assigns  : cmain
+    @ensures  : supprime la première occurence de la carte c de la main cmain et décale l'indice des cartes de sorte à ne pas avoir de carte nulle entre deux cartes non-nulles, renvoie 1 le cas échéant. renvoie 0 si la carte n'était pas dans la main */
+int supprime_carte_main(carte c, carteEnMain cmain, int size);
+
+/*  @requires : cmain tableau de taille size, 0 <= pos < size
+    @assigns  : nothing
+    @ensures  : renvoie la carte de la main cmain située à la position pos, renvoie la carte nulle s'il n'y a plus de carte à la position pos */
+carte get_carte_at(int pos, carteEnMain cmain, int size);
 
 /*---------------------------------Pioche---------------------------------*/
-typedef struct bucket* pioche;
+typedef struct bucket *pioche;
 
 /*  @requires : nothing
     @assigns  : nothing
     @ensures  : retourne une pioche vide */
 pioche cree_pioche_vide();
 
-/*  @requires : l est une pioche valide
+/*  @requires : p est une pioche valide
     @assigns  : nothing
-    @ensures  : retourne 1 si l est vide et 0 sinon */
-int est_vide(pioche l);
+    @ensures  : retourne 1 si p est vide et 0 sinon */
+int est_vide(pioche p);
 
-/*  @requires : pl est un pointeur valide vers une pioche valide
-    @assigns  : *pl
-    @ensures  : ajoute c au dessus de la pioche pl */
-void push(carte c, pioche *pl);
+/*  @requires : pp est un pointeur valide vers une pioche valide
+    @assigns  : *pp
+    @ensures  : ajoute c au dessus de la pioche *pp */
+void push(carte c, pioche* pp);
 
-/*  @requires : pl est un pointeur valide vers une pioche valide non-vide
-    @assigns  : *pl
-    @ensures  : retourne la tête de la pioche et la retire de la pioche */
-carte pop(pioche *pl);
-/*pop = tirer une carte*/
+/*  @requires : pp est un pointeur valide vers une pioche valide non-vide
+    @assigns  : *pp
+    @ensures  : retourne la carte en haut de la pioche et la retire de la pioche*/
+carte pop(pioche* pp); // = tirer une carte;
 
-/*  @requires : l est une pioche valide
+/*  @requires : p est une pioche valide
     @assigns  : nothing
-    @ensures  : print la pioche */
-void print_pioche(pioche l);
+    @ensures  : retourne la carte en haut de la pioche */
+carte peek(pioche p);
+
+/*  @requires : p est une pioche valide, c est une carte valide
+    @assigns  : nothing
+    @ensures  : retourne 1 si c est dans la pioche p, 0 sinon */
+int mem_pioche(pioche p, carte c);
+
+/*  @requires : p est une pioche valide
+    @assigns  : nothing
+    @ensures  : retourne le nombre d'éléments dans la pioche */
+int pioche_len(pioche p);
 
 #endif
