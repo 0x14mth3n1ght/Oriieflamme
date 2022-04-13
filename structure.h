d6f3d658d669274222a9a6d60b8459f2a4b6185b
Julien Zhu <julien.zhu@ensiie.fr>
2022-04-14 01:06:32 +0200

    Tache B3 : Modification .h : La structure des cartes en main devient celle d'une liste chainee ; changement de nom de la liste chainee, changement des fonctions de pioche en fonctions de liste chainee generales

--- structure.h
+++ structure.h
@@ -31,77 +31,63 @@ carte get_carte(plateau g, int x, int y);
 /*  @requires : pg est un pointeur valide vers un plateau valide
     @assigns  : *pg
     @ensures  : supprime la carte du plateau située aux coordonnées (x,y) du plateau *pg (remplacée par la carte nulle), retourne 1 le cas échéant. retourne 0 sinon (pas de carte ou carte déjà supprimée) */
-int supprime_carte_plateau(plateau *pg, int x, int y);
+int supp_carte_plateau(plateau *pg, int x, int y);
 
-/*--------------------------------- Cartes en main ---------------------------------*/
-typedef carte* carteEnMain; /*Tableau 1D de cartes ; un emplacement vide sera caractérisé par la carte nulle*/
-
-/*  @requires : cmain tableau de taille size
-    @assigns  : cmain
-    @ensures  : initialise une main vide composée de size cartes */
-void main_vide(carteEnMain cmain, int size);
-
-/*  @requires : cmain tableau de taille size
-    @assigns  : cmain
-    @ensures  : rajoute la carte c à la main cmain si elle n'est pas pleine, renvoie sa position dans cmain le cas échéant. renvoie 0 si la main était pleine */
-int ajout_carte(carte c, carteEnMain cmain, int size);
-
-/*  @requires : cmain tableau de taille size
-    @assigns  : nothing
-    @ensures  : teste si la carte c est dans la main cmain, renvoie la position de la premiere occurence de la carte le cas échéant. renvoie -1 sinon */
-int indice_carte_main(carte c, carteEnMain cmain, int size); //sert de mem
-
-/*  @requires : cmain tableau de taille size
-    @assigns  : cmain
-    @ensures  : supprime la première occurence de la carte c de la main cmain et décale l'indice des cartes de sorte à ne pas avoir de carte nulle entre deux cartes non-nulles, renvoie 1 le cas échéant. renvoie 0 si la carte n'était pas dans la main */
-int supprime_carte_main(carte c, carteEnMain cmain, int size);
-
-/*  @requires : cmain tableau de taille size, 0 <= pos < size
-    @assigns  : nothing
-    @ensures  : renvoie la carte de la main cmain située à la position pos, renvoie la carte nulle s'il n'y a plus de carte à la position pos */
-carte get_carte_at(int pos, carteEnMain cmain, int size);
-
-/*--------------------------------- Pioche : liste chainee ---------------------------------*/
-typedef struct bucket *pioche;
+/*--------------------------------- Liste chainee de cartes ---------------------------------*/
+typedef struct bucket *liste;
+typedef carte elt; /*valeur des éléments de la liste*/
+/*Structures héritant de la structure de liste*/
+typedef liste pioche;
+typedef liste carteEnMain;
 
 /*  @requires : nothing
     @assigns  : nothing
-    @ensures  : retourne une pioche vide */
-pioche cree_pioche_vide();
+    @ensures  : retourne une liste vide */
+liste cree_liste_vide();
 
-/*  @requires : p est une pioche valide
+/*  @requires : l est une liste valide
     @assigns  : nothing
-    @ensures  : retourne 1 si p est vide et 0 sinon */
-int est_vide(pioche p);
+    @ensures  : retourne 1 si l est vide et 0 sinon */
+int test_vide(liste l);
 
-/*  @requires : pp est un pointeur valide vers une pioche valide
-    @assigns  : *pp
-    @ensures  : ajoute c au dessus de la pioche *pp */
-void push(carte c, pioche* pp);
+/*  @requires : pl est un pointeur valide vers une liste valide
+    @assigns  : *pl
+    @ensures  : ajoute e au dessus de la liste *pl */
+void push(elt e, liste* pl);
 
-/*  @requires : pp est un pointeur valide vers une pioche valide non-vide
-    @assigns  : *pp
-    @ensures  : retourne la carte en haut de la pioche et la retire de la pioche*/
-carte pop(pioche* pp); // = tirer une carte;
+/*  @requires : pl est un pointeur valide vers une liste valide non-vide
+    @assigns  : *pl
+    @ensures  : retourne l'élément en haut de la liste et le retire de la liste*/
+elt pop(liste* pl); // = tirer une carte;
 
-/*  @requires : p est une pioche valide
+/*  @requires : l est une liste valide non-vide
     @assigns  : nothing
-    @ensures  : retourne la carte en haut de la pioche */
-carte peek(pioche p);
+    @ensures  : retourne l'élément en haut de la liste */
+elt peek(liste l);
 
-/*  @requires : p est une pioche valide, c est une carte valide
+/*  @requires : l est une liste valide, e est un élément valide
     @assigns  : nothing
-    @ensures  : retourne 1 si c est dans la pioche p, 0 sinon */
-int mem_pioche(pioche p, carte c);
+    @ensures  : teste si e est dans la liste l et retourne son indice dans la liste le cas échéant, -1 sinon */
+int indice(elt e, liste l);
 
-/*  @requires : p est une pioche valide
+/*  @requires : l est une liste valide
     @assigns  : nothing
-    @ensures  : retourne le nombre d'éléments dans la pioche */
-int pioche_len(pioche p);
+    @ensures  : retourne le nombre d'éléments dans la liste */
+int len(liste l);
 
-/*  @requires : p est une pioche valide
+/*  @requires : l est une liste valide
     @assigns  : nothing
-    @ensures  : retourne une pioche qui contient les cartes de p mais mélangées */
-pioche melanger(pioche p);
+    @ensures  : retourne une liste qui contient les éléments de l mais mélangées */
+liste melanger(liste l);
+
+/*  @requires : pl est un pointeur valide vers une liste valide non-vide
+    @assigns  : *pl
+    @ensures  : supprime la première occurence de l'élément e de la liste*/
+int enlever(elt e, liste* pl);
+
+/*  @requires : l est une liste valide, pos < len(l)
+    @assigns  : nothing
+    @ensures  : renvoie l'élement de la liste l situé à la position pos */
+elt get_at(int pos, liste l);
 
 #endif
