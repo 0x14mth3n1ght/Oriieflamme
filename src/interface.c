bd03b1d2800ac6b5caaa6251bd180a0558b1e1fd
yanis <yanis.feuvre@ensiie.fr>
2022-04-28 14:03:27 +0200

    TACHE B8: Mise en forme du code et petites modifications dues aux différents changements d'arguments des fonctions utilisées

--- src/interface.c
+++ src/interface.c
@@ -23,19 +23,16 @@ void print_ligne(int line_length) {
         }
 }
 
-/*@requires : p est un plateau valide
-@assigns    : nothing
-@ensures    : affiche p*/
 void affiche_plateau(plateau p) {
-    int max_north = taille_north(p);
-    int max_south =taille_south(p);
-    int max_west = taille_west(p);
-    int max_east = taille_east(p);
+    grid g= get_grid(p);
+    int max_north = taille_grille(north,p);
+    int max_south =taille_grille(south,g);
+    int max_west = taille_grille(west,g);
+    int max_east = taille_grille(east,g);
     int i,j;
     faction faction1;
     faction faction2;
-    liste_faction l_fact =retourne_factions(p);
-    //TODO PCQ PAS FINI STRUCT LISTE_FACTION
+    retourne_factions(p,&faction1, &faction2);
     for(i=max_south; i>=max_north; i+=1){
         print_ligne(max_east-max_west);
         print("\n");
@@ -60,7 +57,7 @@ void affiche_plateau(plateau p) {
                 case 1:
                 case 2:{
                     cell cell_to_print= get_cell(p,i,j);
-                    carte card_to_print= get_card(*cell_to_print);
+                    carte card_to_print= get_card(cell_to_print);
                     int num_faction= get_card_faction(cell_to_print);
 
                     char * name_card= get_carte_nom(card_to_print);
@@ -78,41 +75,6 @@ void affiche_plateau(plateau p) {
                     printf("%4s","");
 
                 }
-            
-
-
-/* 
-            if (cachee_visible_existe(p,i,j)==3) { //TODO PAS SUR du &card_to_print
-                printf("%4s","");
-            }
-            cell cell_to_print= get_cell(p,i,j);
-            carte card_to_print= get_card(*cell_to_print);
-            
-            int num_faction= get_card_faction(cell_to_print);
-            if (get_visible(*cell_to_print) == 0 ) {
-
-                if(num_faction==1){
-                    printf("\033[0;31m");
-                    printf("%4s","X");
-                    printf("\033");
-                }
-                else {
-                    printf("%4s","X");
-                }
-            }
-            else {
-                char * name_card= get_carte_nom(card_to_print);
-                if(num_faction==1) {
-                    printf("\033[0;31m");
-                    printf("%4s",name_card);
-                    printf("\033"); 
-                }
-                else{
-                    printf("%4s",name_card);
-
-                }
-            }
- */
             }
         
         }
@@ -123,9 +85,7 @@ void affiche_plateau(plateau p) {
 
 }
 
-/*@requires : f est valide
-@assigns    : nothing
-@ensures    : affiche la main de f*/
+
 void affiche_main(faction f) {
     printf("\n Voici votre main: \n");
     liste main_f= get_main(f);
@@ -147,11 +107,7 @@ void affiche_main(faction f) {
     printf("| \n");
 }
 
-/*@requires : f est valide, les cartes de f ont été piochées, already = O ou 1 (1 si la faction a déjà utilisé l'option)
-@assigns    : nothing
-@ensures    : si already==1, alors renvoie 0, sinon si already == 0, demande à f une des actions suivantes, et renvoie la valeur associée :
-    0. conserve sa main
-    1. vide sa main, mélange la pioche, repioche sa nouvelle main*/
+
 int mulligan_main(faction f, int already){
     if (1==already){
         return 0;
@@ -172,9 +128,6 @@ int mulligan_main(faction f, int already){
     return(1);
 }
 
-/*@requires : f est valide
-@assigns    : nothing
-@ensures    : demande à f la carte qu'elle souhaite poser sur le plateau et la retourne*/
 carte choix_carte(faction f){
     liste main_f= get_main(f);
     int length_main= len_liste(main_f);
@@ -192,10 +145,6 @@ carte choix_carte(faction f){
     }
     return result;
 }
-
-/*@requires : f est valide, pos est un tableau d'entiers positifs de taille 2
-@assigns    : pos
-@ensures    : demande à f les coordonnées (x,y) de l'endroit où elle souhaite poser la carte. Les coordonnées seront stockées dans le tableau pos = [x,y]*/
 void position_carte(faction f, int* x,int* y){
         printf("\n Où voulez vous poser votre carte? \n Rappel: la carte doit être adjacente à un autre carte déjà posée");
         printf("Entrez la coordonée x:\n");
@@ -208,16 +157,10 @@ void position_carte(faction f, int* x,int* y){
         *y=answer_y;
 }
 
-/*@requires : c est valide
-@assigns    : nothing
-@ensures    : affiche les effets d'une carte c (qui vient d'être retournée sur le plateau)*/
 void affiche_effet(carte c){
     printf("Voici la description de la carte qui vient d'être retournée:\n%s",get_carte_description(c));
 }
 
-/*@requires : f1 et f2 valides
-@assigns    : nothing
-@ensures    : affiche le vainqueur de la partie et retourne 1 ou 2 le numéro de la faction gagnante*/
 int affiche_gagnant(faction f1, faction f2){
     int ddrs1,ddrs2;
     ddrs1=get_ddrs(f1);
